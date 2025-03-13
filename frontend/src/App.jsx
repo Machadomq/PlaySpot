6439bdeb802b25c9f726f55d8c460df11691ba35
Machadomq <gbrielmq1213@gmail.com>
2025-03-13 14:07:00 -0300

    arrumando o header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import logo from './assets/logo.png'
+import usericon from './assets/usericon.png'
 
 function App() {
   return (
     <>
-      <header className="header">
-        PlaySpot
+      <header>
+        <p className='titulo'>PlaySpot</p>
+        <img className='usericon' src={usericon} alt="" />
       </header>
+
       <div className="container">
         <img src={logo} alt="Logo PlaySpot" />
       </div>
