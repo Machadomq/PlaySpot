0babf2477f3b9359657d9fd0296c54ac4df601f9
Machadomq <gbrielmq1213@gmail.com>
2025-03-18 13:50:28 -0300

    atualizando o eclipse

--- PlaySpot_Eclipse/program/src/main/java/com/playspot/backend/controllers/HelloController.java
+++ PlaySpot_Eclipse/program/src/main/java/com/playspot/backend/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controllers;
+package com.playspot.backend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
